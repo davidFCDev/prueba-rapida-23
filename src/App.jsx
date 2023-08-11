db13c16cf972557ce3122b64424a057a68b890bd
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-11 13:51:09 +0200

    hook y errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useState, useId } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
+import { useEffect } from "react";
 
-function App() {
+function useSearch() {
   const [search, updateSearch] = useState("");
   const [error, setError] = useState(null);
+  const isFirstInput = useId(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search, isFirstInput]);
+
+  return { search, updateSearch, error };
+}
+
+function App() {
+  const { search, updateSearch, error } = useSearch();
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
