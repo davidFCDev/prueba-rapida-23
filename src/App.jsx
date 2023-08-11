b3fc2e6c24b82d7c2a7142391651a525f61071e5
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-11 13:47:16 +0200

    control eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
   const movies = responseMovies.Search;
+
+  const mappedMovies = movies.map((movie) => {
+    return {
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster,
+    };
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Buscando películas...");
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input type="text" placeholder="Busca una película" />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            type="text"
+            placeholder="Busca una película"
+          />
           <button>Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={movies} />
+        <Movies movies={mappedMovies} />
       </main>
     </div>
   );
