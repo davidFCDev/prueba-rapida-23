32c7fd1baedf38d18b6cc85cfa4a614d3634bbb9
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-11 13:42:59 +0200

    render movies mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
 
 function App() {
-  return <div></div>;
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form>
+          <input type="text" placeholder="Busca una película" />
+          <button>Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <img src={movie.Poster} alt={movie.Title} />
+                <h2>{movie.Title}</h2>
+                <p>{movie.Year}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No hay películas</p>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
